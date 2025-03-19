6d88f6783a96e097a8f85661058adfa51052a5ac
rezaforwork08 <rezaforwork08@gmail.com>
2025-03-19 14:43:17 +0700

    add row in dashboard

--- dashboard.php
+++ dashboard.php
@@ -99,7 +99,9 @@ include 'koneksi.php';
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script>
         $('.add-row').click(function() {
-            alert('Testing');
+            let newRow = "";
+            newRow += "<tr>"
+            newRow += "</tr>"
         });
     </script>
 </body>
